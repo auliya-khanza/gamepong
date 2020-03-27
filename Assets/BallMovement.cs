2872fd5553d997ff77195df5ca84f930f101993e
auliya-khanza <62418869+auliya-khanza@users.noreply.github.com>
2020-03-27 19:51:17 +0700

    update ball, timer, dan jeda

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public int speed=20;
+    // Start is called before the first frame update
+    public Rigidbody2D sesuatu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sesuatu.velocity = new Vector2(-1,-1) * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        if (other.collider.name=="WallRight"||other.collider.name=="WallLeft")
+        {
+                StartCoroutine(jeda());
+        }
+    }
+
+    IEnumerator jeda(){
+        sesuatu.velocity = Vector2.zero;
+        sesuatu.GetComponent<Transform>().position = Vector2.zero;
+        yield return new WaitForSeconds(1);
+        sesuatu.velocity = new Vector2(-1,-1) * speed;
+    }
+}
